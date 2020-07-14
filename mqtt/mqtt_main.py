428dfcbe76072b32159e7258b104703071521adf
soleyj <48433045+soleyj@users.noreply.github.com>
2020-07-14 20:00:38 +0200

    minor bug

--- mqtt/mqtt_main.py
+++ mqtt/mqtt_main.py
@@ -11,27 +11,29 @@ def on_message(client, userdata, message):
     
     payload = str(message.payload.decode("utf-8", "ignore"))
     payload_json = json.loads(payload)
-    print(payload_json)   
     if(message.topic == "homegrown/inputs"):
+        machine = machine_hw.objects.get(id_machine = payload_json["id"])
         new_model = data_hg()
-        new_model.machine = machine_hw.objects.get(id_machine = payload_json["id"])
+        new_model.machine = machine
         new_model.temp_1 = payload_json["temp_1"]
         new_model.humid_1 = payload_json["humid_1"]
         new_model.sw_1 = payload_json["sw_1"]
         new_model.sw_2 = payload_json["sw_2"]
         new_model.save()
-
+        print(payload_json["out_12"][0])
         new_outs_model = data_hg_outputs()
-        new_outs_model.Ventilador =     payload_json["out_12"][0]
-        new_outs_model.Bomba_1 =     payload_json["out_12"][1]
-        new_outs_model.Bomba_2 =     payload_json["out_12"][2]
-        new_outs_model.Bomba_3 =     payload_json["out_12"][3]
-        new_outs_model.Bomba_3 =     payload_json["rele"][0]
-        new_outs_model.Bomba_h20 =     payload_json["rele"][1]
-        new_model.machine = machine_hw.objects.get(id_machine = payload_json["id"])
-
+        new_outs_model.Ventilador = payload_json["out_12"][0]
+        new_outs_model.Bomba_1 = payload_json["out_12"][1]
+        new_outs_model.Bomba_2 = payload_json["out_12"][2]
+        new_outs_model.Bomba_3 = payload_json["out_12"][3]
+        new_outs_model.Llum = payload_json["rele"][0]
+        new_outs_model.Bomba_h20 = payload_json["rele"][1]
+        new_outs_model.Altura = 0
+        new_outs_model.machine = machine
+        print("in")
         new_outs_model.save()
-        pass
+        print("in")
+
 
 
 def on_connect(client, userdata, flags, rc):
